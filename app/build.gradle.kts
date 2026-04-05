c9d583d84669bfe1c14883d95c0baf7e02a5cc3b
FelixJP23 <felixsh230517@gmail.com>
2026-04-05 10:21:23 -0300

    Update targetSdk from 34 to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     defaultConfig {
         applicationId = "com.example.ownmyway"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
