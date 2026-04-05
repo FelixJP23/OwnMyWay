891083f099f4ca9d1e3de3ea22971aa4e8d69876
ArturML01 <correa.arturanderson@gmail.com>
2026-04-05 02:07:27 -0300

    Tela Login v4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
     // buildFeatures must be here, inside android{} but OUTSIDE defaultConfig{}
     buildFeatures {
         buildConfig = true
+        compose = false
     }
 
     buildTypes {
@@ -67,15 +68,23 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    // Testes
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     implementation(libs.androidx.espresso.core)
+
+    // Ciclo de vida e Corrotinas
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+
+    // Google Maps
     implementation("com.google.android.gms:play-services-maps:20.0.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
+
+    // Supabase v3
     implementation(platform("io.github.jan-tennert.supabase:bom:3.1.4"))
     implementation("io.github.jan-tennert.supabase:postgrest-kt")
     implementation("io.github.jan-tennert.supabase:auth-kt")
-    implementation("io.ktor:ktor-client-android:3.1.1")
+    implementation("io.ktor:ktor-client-android:3.0.1")
 }
